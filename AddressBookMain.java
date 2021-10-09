26ff3a3e9eadc33072529e566822a573d1a4dcad
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-09 17:44:58 +0530

    Added: New Contact in AddressBookSystem

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,16 +4,27 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 
-		PersonDetails person = new PersonDetails();
-		person.FirstName("Rahul");
-		person.LastName("Labade");
-		person.Address("Arjun_Nagar");
-		person.City("Amravati");
-		person.State("Maharashtra");
-		person.Zip("456123");
-		person.PhoneNumber("123456789");
-		person.Email("rahullabade05@gmail.com");
-		System.out.println(person.toString());
+		PersonDetails personOne = new PersonDetails();
+		personOne.FirstName("Rahul");
+		personOne.LastName("Labade");
+		personOne.Address("Arjun_Nagar");
+		personOne.City("Amravati");
+		personOne.State("Maharashtra");
+		personOne.Zip("456123");
+		personOne.PhoneNumber("123456789");
+		personOne.Email("rahullabade05@gmail.com");
+		System.out.println(personOne.toString());
+		
+		PersonDetails personTwo = new PersonDetails();
+		personTwo.FirstName("Nikhil");
+		personTwo.LastName("Labade");
+		personTwo.Address("Arjun_Nagar");
+		personTwo.City("Amravati");
+		personTwo.State("Maharashtra");
+		personTwo.Zip("456123");
+		personTwo.PhoneNumber("123456789");
+		personTwo.Email("Nikhillabade05@gmail.com");
+		System.out.println(personTwo.toString());
 	}
 
 }
