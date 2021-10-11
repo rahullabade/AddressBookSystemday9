b15b51ccc1ed177e8c8cb76979fdc516c65f0969
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-11 18:36:57 +0530

    Refactored: New Contact in AddressBookSystem

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,30 +1,32 @@
 package com.bl.AddressBookSystem;
 
 public class AddressBookMain {
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 
 		PersonDetails personOne = new PersonDetails();
-		personOne.FirstName("Rahul");
-		personOne.LastName("Labade");
-		personOne.Address("Arjun_Nagar");
-		personOne.City("Amravati");
-		personOne.State("Maharashtra");
-		personOne.Zip("456123");
-		personOne.PhoneNumber("123456789");
-		personOne.Email("rahullabade05@gmail.com");
+		personOne.setFirstName("Rahul");
+		personOne.setLastName("Labade");
+		personOne.setAddress("Arjun_Nagar");
+		personOne.setCity("Amravati");
+		personOne.setState("Maharashtra");
+		personOne.setZip("456123");
+		personOne.setPhoneNumber("123456789");
+		personOne.setEmail("rahullabade05@gmail.com");
 		System.out.println(personOne.toString());
-		
+
 		PersonDetails personTwo = new PersonDetails();
-		personTwo.FirstName("Nikhil");
-		personTwo.LastName("Labade");
-		personTwo.Address("Arjun_Nagar");
-		personTwo.City("Amravati");
-		personTwo.State("Maharashtra");
-		personTwo.Zip("456123");
-		personTwo.PhoneNumber("123456789");
-		personTwo.Email("Nikhillabade05@gmail.com");
+		personTwo.setFirstName("Nikhil");
+		personTwo.setLastName("Labade");
+		personTwo.setAddress("Arjun_Nagar");
+		personTwo.setCity("Amravati");
+		personTwo.setState("Maharashtra");
+		personTwo.setZip("456123");
+		personTwo.setPhoneNumber("123456789");
+		personTwo.setEmail("Nikhillabade05@gmail.com");
 		System.out.println(personTwo.toString());
+
 	}
 
 }
--- PersonDetails.java
+++ PersonDetails.java
@@ -10,35 +10,67 @@ public class PersonDetails {
 	private String phoneNumber;
 	private String email;
 
-	public void FirstName(String firstName) {
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	public void LastName(String lastName) {
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	public void Address(String address) {
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
 		this.address = address;
 	}
 
-	public void City(String city) {
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
 		this.city = city;
 	}
 
-	public void State(String state) {
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
 		this.state = state;
 	}
 
-	public void Zip(String zip) {
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
 		this.zip = zip;
 	}
 
-	public void PhoneNumber(String phoneNumber) {
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
-	public void Email(String email) {
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
@@ -48,5 +80,5 @@ public class PersonDetails {
 				+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
 				+ "]";
 	}
-	
+
 }
